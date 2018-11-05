76804975525da3ef39013a6edc0ada829d2d8f83
Yuki N <lunarbase@outlook.com>
2018-11-05 00:49:03 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
- your_hash["apple"] = 50
+ my_hash["apple"] = 50
   
 end
 
